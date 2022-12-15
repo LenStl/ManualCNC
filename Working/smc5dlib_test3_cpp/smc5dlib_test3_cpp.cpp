2c24689171426d606d49ee8988e7fdf2fbee6264
LenStl <s6ltstel@uni-bonn.de>
2022-12-15 14:19:01 +0100

    Did more, but git didnt like it. Noted everything that needs to be done.
    Signed-off-by: LenStl <s6ltstel@uni-bonn.de>

--- Working/smc5dlib_test3_cpp/smc5dlib_test3_cpp.cpp
+++ Working/smc5dlib_test3_cpp/smc5dlib_test3_cpp.cpp
@@ -5,9 +5,11 @@
 #include <array>
 
 using namespace System;
+using namespace De::Boenigk::SMC5D::Settings;
 using namespace De::Boenigk::SMC5D::Basics;
 using namespace De::Boenigk::SMC5D::Move;
 
+// Add "waitForReady(connector, etc.)" function that sleeps until status is ready
 
 int main(array<System::String ^> ^args)
 {
@@ -39,7 +41,14 @@ int main(array<System::String ^> ^args)
         return false;
     }
 
-    // New;
+    // Create Reference, SpeedTact, Job with Steps from 50-100, z = 100, speed, about 20, 30
+
+
+    // Do Reference
+    // Do Positioning (with Manual Move and Sleep)
+    // Create Job with z= 100, move speed = 20, square between 50 - 100
+    // Manipulate with Key Inputs. Speed control via SpeedTact. Job also has .Pause and .Continue, but dont work properly.
+
     myManualMove->Step(MoveAxis::Y, false, 10.0f);
     Sleep(5000);
     myManualMove->Stop();
